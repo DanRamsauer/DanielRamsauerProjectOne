41c9bf43c167759f41aa15f9a3d60fe76f215e3c
DanielRamsauer <danramsauer@gmail.com>
2022-07-11 18:23:26 -0400

    finished hamburger menu

--- scripts.js
+++ scripts.js
@@ -5,11 +5,10 @@
 
 // Step 1 - Getting the hamburger menu icon and the actual slide out nav and storing them in a variable
 const hamburgerMenuImg = document.querySelector('img');
-const slideOutNav = document.getElementsByClassName('slideOutNav');
+const slideOutNav = document.querySelector('.slideOutNav');
 
 // Step 2 - Adding a listen event to the hamburgermenu img and using a click
 hamburgerMenuImg.addEventListener('click', function () {
     // Step 3 - Toggling a class of hamburgermenu so it can change the left value to 0 and back to -506 so it comes on and off the screen
-    slideOutNav[0].classList.toggle('hamburgerMenu')
-    console.log(slideOutNav[0]);
+    slideOutNav.classList.toggle('hamburgerMenu')
 });
